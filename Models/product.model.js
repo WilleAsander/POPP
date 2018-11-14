2adcfe3cfc1314550791c428a7e2594461fc99c9
Testo12 <erik.malmsten99@gmail.com>
2018-11-14 16:39:18 +0100

    added category for item

--- Models/product.model.js
+++ Models/product.model.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
     name: {type: String, required: true, max: 100},
+    description: {type : String, required : false , max: 200},
+    category: {type: String, required : false , max:200},
     price: {type: Number, required: true}
+
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
