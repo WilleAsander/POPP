7e6b8f188d265c38314324eb1f2093ca76a330b1
RyuTribal <ivan.sedelkin9@gmail.com>
2018-11-14 13:27:34 +0100

    Added a new call function called hello

--- Controllers/home.controller.js
+++ Controllers/home.controller.js
@@ -1,5 +1,9 @@
 var path = require('path');
 
+exports.test = function(req,res){
+    res.send("Hello from MVC and Node");
+}
+
 exports.home = function(req,res){
     res.sendFile(path.resolve('www/index.html'));
 }
