93e297abafb5da5dd767503675a9f5fc9ed19b77
Testo12 <erik.malmsten99@gmail.com>
2018-11-14 13:16:58 +0100

    Added old code from yesterday

--- Routes/product.route.js
+++ Routes/product.route.js
@@ -2,3 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var controller = require('../Controllers/product.controller');
+
+router.post('/create', product_controller.create);
+router.get('/:id', product_controller.details);
+router.put('/:id/update', product_controller.update);
+router.delete('/:id/delete', product_controller.delete )
+module.exports = router;
