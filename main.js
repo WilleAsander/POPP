8e15492b336ceed58fdec696ad442ee9aabec3c4
RyuTribal <ivan.sedelkin9@gmail.com>
2018-11-15 11:20:57 +0100

    Added a delete button

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-   //Skriver text i index.html
-   var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4) {
-            document.getElementById('kvack').innerHTML = this.responseText;
+            var myArr = JSON.parse(this.responseText);
+             myFunction(myArr)
+            }
+        };
+
+    xhr.open('GET', 'https://glacial-shore-82418.herokuapp.com/products', true);
+    xhr.send(); 
+    function myFunction(arr){
+        var out = "";
+        var i;
+        for(i = 0; i < arr.length; i++){
+            out += '<tr> <td>' + arr[i].name + '</td>' + " " + '<td>' + arr[i].price + '</td> </tr>'
+            +" "+'<td> <a href="https://glacial-shore-82418.herokuapp.com/products/'+ arr[i]._id+'">Details</a>';
         }
-    };
-
-    xhr.open('GET', 'https://glacial-shore-82418.herokuapp.com/hello', true);
-    xhr.send();
-
+        document.getElementById("table").innerHTML += out;
+}
 
     
